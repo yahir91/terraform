9774b78b4d4db4ce7cd3418c46809d53e90ada9b
Yahir Cardona <yahircardona@Yahirs-MacBook-Pro-78.local>
2023-09-08 10:53:21 -0600

    chore: make use of functions

--- methods/methods.tf
+++ methods/methods.tf
@@ -2,16 +2,20 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+locals {
+    count = var.is_test ? 1 : 0
+}
+
 resource "aws_instance" "dev" {
    ami = "ami-082b5a644766e0e6f"
    instance_type = var.instance_type
-   count = var.is_test == true ? 1 : 0
+   count = local.count
 }
 
 resource "aws_instance" "prod" {
    ami = "ami-082b5a644766e0e6f"
    instance_type = var.instance_type
-   count = var.is_test == true ? 0 : 1
+   count = local.count
 }
 
 resource "aws_iam_user" "name" {
